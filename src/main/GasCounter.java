8590c0adc001627efb92c5f00c6390a0f5f620b9
league <league@192.168.1.200>
2024-02-24 14:34:27 -0800

    Updated to use scanner.

--- src/main/GasCounter.java
+++ src/main/GasCounter.java
@@ -1,20 +1,29 @@
 package main;
 
+import java.util.Scanner;
+
 import javax.swing.JOptionPane;
 
 public class GasCounter {
 public static void main(String[] args) {
-JOptionPane.showMessageDialog(null, "This is the gas calculator. This doohickey will tell you if you can drive a hundred miles!");
-String gallonString = JOptionPane.showInputDialog(null, "How much gas in gallons do you have?");
+//JOptionPane.showMessageDialog(null, "This is the gas calculator. This doohickey will tell you if you can drive a hundred miles!");
+Scanner scan = new Scanner(System.in);
+
+System.out.println("This is the gas calclulator. This doohickey will tell you if you can drive a hundred miles!");
+//String gallonString = JOptionPane.showInputDialog(null, "How much gas in gallons do you have?");
+System.out.println("how much gas in gallons do you have?");
+String gallonString = scan.nextLine();
 System.out.println("How much gas do you have");
 double gallonAmt = (double) Double.parseDouble(gallonString);
 System.out.println(gallonAmt);
-String efficiencyString = JOptionPane.showInputDialog(null, "how many miles per gallon?");
+//String efficiencyString = JOptionPane.showInputDialog(null, "how many miles per gallon?");
 System.out.println("How many miles per gallon?");
+String efficiencyString = scan.nextLine();
 double efficiency = (double) Double.parseDouble(efficiencyString);
 System.out.println(efficiency);
-String priceString = JOptionPane.showInputDialog(null, "what is the price of gas?");
+//String priceString = JOptionPane.showInputDialog(null, "what is the price of gas?");
 System.out.println("what is the price of gas?");
+String priceString = scan.nextLine();
 double price = (double) Double.parseDouble(priceString);
 System.out.println(price);
 double gallons100Miles = (100/efficiency);
@@ -24,7 +33,7 @@ System.out.println("Your distance in miles on a full tank of gas is " + distance
 double priceFullTank = (price * gallonAmt);
 System.out.println("Your price for a full tank is $" + String.format("%.2f", priceFullTank));
 if (distanceFullTank > 100) {
-	System.out.println("AW YEAH!! Your car can go 100 miles}");}
+	System.out.println("AW YEAH!! Your car can go 100 miles.");}
  if (distanceFullTank < 100) {
 	 System.out.println("Oh... Your car can't go 100 miles.");}
 }
